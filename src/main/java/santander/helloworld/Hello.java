7d23167d6738c55d164fd16b96df5ab260e2c120
Jose Manuel Alvarez Martinez <ztkx@hotmail.com>
2018-08-23 13:57:01 -0500

    Primer commit

---
+++ src/main/java/santander/helloworld/Hello.java
@@ -0,0 +1,13 @@
+package santander.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hello
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
